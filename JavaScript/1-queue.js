4e5bacec9e38ca48c783f4ddc99d32fabb6ee143
Andry Savchuk IM-24 <deadpoulll@gmail.com>
2025-06-12 16:12:25 +0300

    Optimize 1-queue.js with modern JS practices

--- JavaScript/1-queue.js
+++ JavaScript/1-queue.js
@@ -1,18 +1,61 @@
 'use strict';
 
-class Queue {
-  #buffer = [];
 
-  get length() {
-    return this.#buffer.length;
+const _items = Symbol('items');
+const _size = Symbol('size');
+
+class Queue {
+  constructor() {
+
+    this[(_items)] = []; 
+    this[(_size)] = 0;   
   }
+
 
   enqueue(item) {
-    this.#buffer.push(item);
+    
+    this[(_items)].push(item);
+    this[(_size)]++;
   }
 
+  
   dequeue() {
-    return this.#buffer.shift();
+    if (this[(_size)] === 0) {
+      return undefined;
+    }
+    this[(_size)]--;
+    return this[(_items)].shift();
+  }
+
+  peek() {
+    if (this[(_size)] === 0) {
+      return undefined;
+    }
+    return this[(_items)][0];
+  }
+
+  isEmpty() {
+    return this[(_size)] === 0;
+  }
+
+  length() {
+    return this[(_size)];
+  }
+
+  clear() {
+    this[(_items)] = [];
+    this[(_size)] = 0;
+  }
+
+  *[Symbol.iterator]() {
+    
+    for (const item of this[_items]) {
+      yield item;
+    }
+  }
+
+  toArray() {
+    return [...this[(_items)]]; 
   }
 }
 
